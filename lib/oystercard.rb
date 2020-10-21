66b65f23ad8259eafa429294bf313c40604af5f7
KalindiFonda <kalindi.fonda@gmail.com>
2020-10-21 16:11:59 +0100

    Q11: Add entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,33 +1,31 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
   MIN_FARE = 1
   MAX_BALANCE = 90
 
   def initialize
     @balance = 0
-    @in_use = false
+    @entry_station = nil
   end
 
   def top_up(amount)
     raise("Maximum limit reached #{MAX_BALANCE}") if @balance + amount > MAX_BALANCE
-
     @balance += amount
   end
 
   def in_journey?
-    @in_use
+    !!@entry_station
   end
 
-  def touch_in
+  def touch_in(station)
     raise("Insufficient funds!") if @balance < MIN_FARE
-
-    @in_use = true
+    @entry_station = station.name
   end
 
   def touch_out
     @balance -= MIN_FARE
-    @in_use = false
+    @entry_station = nil
   end
 
   private
